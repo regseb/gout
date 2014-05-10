090166d321d9a165a4a26b4fd4840b662c09c614
regseb <regseb@gmail.com>
2014-05-10 12:10:22 +0200

    Correction de l'extract de Ufunk.

--- gate/regseb/std/image/ufunk/extract.js
+++ gate/regseb/std/image/ufunk/extract.js
@@ -2,8 +2,11 @@ function(data) {
     "use strict";
     return $.get("http://www.ufunk.net/").then(function(data) {
         var article = $("#header3 a:first", data);
+        var src = $("img", article).attr("src");
+        src = src.substr(src.indexOf("src=") + 4);
+        src = src.substr(0, src.indexOf("&"));
         return {
-            "img":   $("img", article).attr("src"),
+            "img":   src,
             "link":  $(article).attr("href"),
             "title": $("h2", article).text()
         };
