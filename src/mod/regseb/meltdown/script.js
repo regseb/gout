ac5ad516643478dcba62cf38ffbbdd3ab19257d6
Sébastien Règne <regseb@gmail.com>
2016-03-31 23:13:52 +0200

    [regseb/meltdown] Afficher tous les évènements du jour.

--- src/mod/regseb/meltdown/script.js
+++ src/mod/regseb/meltdown/script.js
@@ -8,18 +8,24 @@ define(["jquery", "scronpt"], function ($, Cron) {
     const extract = function (city) {
         const url = "http://www.meltdown.bar/" + city + "/planning";
         return $.get(url).then(function (data) {
-            const $data = $("#event-detail_0 .event-detail:first", data);
-
-            return {
-                "title": $("h3", $data).text(),
-                "desc":  $(".event-detail-text", $data).html()
-            };
+            const day = new Date().getDay() + 6 % 7;
+            const $data = $("#event-detail_0" + day, data);
+            return $(".event-detail", $data).map(function () {
+                return {
+                    "title": $("h3", this).text(),
+                    "desc":  $("p", this).text(),
+                    "hour":  $("span:first", this).text().trim()
+                };
+            }).get();
         });
     }; // extract()
 
     const display = function ($root, data) {
-        $("a", $root).text(data.title);
-        $("span", $root).html(data.desc);
+        $("a", $root).text(data.map(event => event.title).join(" / "));
+        $("span", $root).html(data.map(function (event) {
+            return "<strong>" + event.title + "</strong> (" + event.hour +
+                   ") : " + event.desc;
+        }).join("<br />"));
     }; // display()
 
     const update = function (id) {
