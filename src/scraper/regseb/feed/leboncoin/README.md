babe483e49f6231a40f849aa5413f5b4d27e5cca
Sébastien Règne <regseb@gmail.com>
2016-09-07 19:09:09 +0200

    Ajouter un scraper pour leboncoin.

---
+++ src/scraper/regseb/feed/leboncoin/README.md
@@ -0,0 +1,30 @@
+# /regseb/feed/leboncoin
+
+Ce scraper récupère la liste des dernières annonces postées sur
+**[leboncoin](//www.leboncoin.fr/)** selon des critères.
+
+## Configuration
+
+La configuration contient l'URL d'une recherche.
+
+## Exemple
+
+Cet exemple affiche les dernières annonces de vente de
+*[Clio](//www.leboncoin.fr/voitures/offres/centre/?th=1&q=clio)*.
+
+```JSON
+{
+    "std/feed/leboncoin": {
+        "widget": "std/feed",
+        "coord": { "x": 1, "y": 1, "w": 20, "h": 5 },
+        "config": {
+            "color": "#ff5722",
+            "cron": "0 * * * *"
+        },
+        "scrapers": [
+            { "scraper": "regseb/feed/leboncoin",
+              "config": "https://www.leboncoin.fr/voitures/offres/centre/?th=1&q=clio" }
+        ]
+    }
+}
+```
---
+++ src/scraper/regseb/feed/leboncoin/script.js
@@ -0,0 +1,45 @@
+define(["jquery"], function ($) {
+    "use strict";
+
+    const MONTHS = ["janvier", "février", "mars", "avril", "mai", "juin",
+                    "juillet", "août", "sept", "octobre", "novembre",
+                    "décembre"];
+    return class {
+        constructor(url) {
+            this.url = url;
+        } // constuctor()
+
+        list(size) {
+            return $.get(this.url).then(function (data) {
+                const items = [];
+                $(".tabsContent a:lt(" + size + ")", data).each(function () {
+                    const parts = $(".item_absolute", this).text()
+                                                          .replace("Urgent", "")
+                                                          .trim()
+                                                          .split(/[,:] */);
+                    const date = new Date();
+                    if ("Aujourd'hui" === parts[0]) {
+                        // Ne rien faire.
+                    } else if ("Hier" === parts[0]) {
+                        date.setDate(date.getDate() - 1);
+                    } else {
+                        const subparts = parts[0].split(" ");
+                        date.setMonth(MONTHS[subparts[1]]);
+                        date.setDate(parseInt(subparts[0], 10));
+                    }
+                    date.setHours(parseInt(parts[1], 10));
+                    date.setMinutes(parseInt(parts[2], 10));
+
+                    items.push({
+                        "title": $(this).attr("title"),
+                        "desc":  "",
+                        "link":  $(this).attr("href"),
+                        "guid":  $(this).attr("href"),
+                        "date":  date.getTime()
+                    });
+                });
+                return items;
+            });
+        } // list()
+    };
+});
