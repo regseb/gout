9166b0ef97543fe7978ca96a6c062b3eb68b0700
Sébastien Règne <regseb@gmail.com>
2016-09-08 20:07:44 +0200

    [std/button] Améliorer l'effet du survol.

--- src/widget/std/button/script.js
+++ src/widget/std/button/script.js
@@ -3,14 +3,12 @@ define(["jquery"], function ($) {
 
     const create = function (id, url, config) {
         const $root = $("#" + id);
-        $root.css({
-            "background-color": config.color,
-            "background-image": "url(\"" + url + "/icon.svg\")"
-        });
+        $root.css("background-color", config.color);
         $("a", $root).attr("href", config.link);
         if ("desc" in config) {
             $("a", $root).attr("title", config.desc);
         }
+        $("img", $root).attr("src", url + "/icon.svg");
     }; // create()
 
     return create;
