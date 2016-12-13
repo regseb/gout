88a06dabee02e0f68fa9b85427d328543fbcb17b
Sébastien Règne <regseb@gmail.com>
2016-12-13 21:05:23 +0100

    [standalone/notepad] Préfixer par le nom du widget dans la clé de localStorage.

--- src/widget/standalone/notepad/script.js
+++ src/widget/standalone/notepad/script.js
@@ -1,21 +1,30 @@
 define(["jquery"], function ($) {
     "use strict";
 
+    const save = function (id, value) {
+        localStorage.setItem("widget/standalone/notepad/" + id, value);
+    }; // save()
+
+    const load = function (id) {
+        return localStorage.getItem("widget/standalone/notepad/" + id);
+    }; // load()
+
     const change = function () {
-        localStorage.setItem($(this).attr("data-url"), $(this).val());
+        const $root = $(this).closest("article");
+
+        save($root.attr("id"), $(this).val());
     }; // change()
 
     const create = function (id, { "config.json": config, "icon.svg": icon }) {
         const $root = $("#" + id);
         $root.css({
-            "background-color": config.color,
+            "background-color": config.color || "black",
             "background-image": "url(\"data:image/svg+xml;base64," +
                                 btoa(icon) + "\")"
         });
-        $("textarea", $root).val(localStorage.getItem(id))
+        $("textarea", $root).val(load(id))
                             .attr({ "title":       config.desc,
-                                    "placeholder": config.title,
-                                    "data-url":    id })
+                                    "placeholder": config.title })
                             .css("border-color", config.color || "black")
                             .change(change);
     }; // create()
