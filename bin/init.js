99d1d8b8af412bb7102fcbf7e78165a1b9b4d2bb
Sébastien Règne <regseb@gmail.com>
2018-02-04 13:23:42 +0100

    Permettre de modifier toutes les propriétés du manifeste.

--- bin/init.js
+++ bin/init.js
@@ -5,15 +5,35 @@
 const fs   = require("fs");
 const path = require("path");
 
+const merge = function (first, second) {
+    if ("object" !== typeof first || "object" !== typeof second) {
+        return second;
+    }
+    const third = {};
+    for (const key of new Set([...Object.keys(first),
+                               ...Object.keys(second)])) {
+        // Si la propriété est dans les deux objets.
+        if (key in first && key in second) {
+            third[key] = merge(first[key], second[key]);
+        // Si la propriété est seulement dans le premier objet.
+        } else if (key in first) {
+            third[key] = first[key];
+        // Si la propriété est seulement dans le second objet.
+        } else {
+            third[key] = second[key];
+        }
+    }
+    return third;
+};
+
 const ROOT_DIR = path.join(__dirname, "..");
 const SRC_DIR  = path.join(ROOT_DIR, "src");
 
 const config = require(path.join(ROOT_DIR, "config"));
-const manifest = require(path.join(SRC_DIR, "manifest-vanilla"));
+let manifest = require(path.join(SRC_DIR, "manifest-vanilla"));
 
-if (null !== config.name) {
-    manifest.name += " - " + config.name;
-    manifest["browser_action"]["default_title"] += " - " + config.name;
+if ("manifest" in config) {
+    manifest = merge(manifest, config.manifest);
 }
 
 // Gérer les propriétés pour Firefox.
