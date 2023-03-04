d0de0f947c53fb594cdd61956952b297dd226114
Sébastien Règne <regseb@gmail.com>
2023-03-04 13:16:06 +0100

    fix(extension): Modifier la requête HTTP.

--- src/extension/background/script.js
+++ src/extension/background/script.js
@@ -2,17 +2,33 @@
  * @module
  */
 
+const replace = (headers, replacements) => {
+    const names = new Set(replacements.map((r) => r.name.toLowerCase()));
+    return [
+        ...headers.filter((h) => !names.has(h.name.toLowerCase())),
+        ...replacements,
+    ];
+};
+
+browser.webRequest.onBeforeSendHeaders.addListener(async (details) => {
+    const { dashboards } = await browser.storage.sync.get("dashboards");
+    if (dashboards.some((d) => details.documentUrl === d.url)) {
+        return {
+            requestHeaders: replace(details.requestHeaders, [
+                { name: "Sec-Fetch-Mode", value: "navigate" },
+            ]),
+        };
+    }
+    return {};
+}, { urls: ["<all_urls>"] }, ["blocking", "requestHeaders"]);
+
 browser.webRequest.onHeadersReceived.addListener(async (details) => {
     const { dashboards } = await browser.storage.sync.get("dashboards");
     if (dashboards.some((d) => details.documentUrl === d.url)) {
         return {
-            responseHeaders: [
-                ...details.responseHeaders.filter((header) => {
-                    return "access-control-allow-origin" !==
-                           header.name.toLowerCase();
-                }),
-                { name: "Access-Control-Allow-Origin",  value: "*" },
-            ],
+            responseHeaders: replace(details.responseHeaders, [
+                { name: "Access-Control-Allow-Origin", value: "*" },
+            ]),
         };
     }
     return {};
